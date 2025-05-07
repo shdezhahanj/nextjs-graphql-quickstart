1b1a4ff78c2acb3b96ebd6326cf67e71fc87e01b
Mohammad Karimi <sw.karimi@gmail.com>
2025-05-07 21:29:18 +0330

    style: formatting

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,8 @@ export { Typography } from './atoms/Typography/Typography';
 export { Header } from './molecules/Header/Header';
 export { Footer } from './molecules/Footer/Footer';
 export { Input } from './molecules/Input/Input';
+export { Modal } from './molecules/Modal/Modal';
+export { TransactionForm } from './molecules/TransactionForm/TransactionForm';
+
+export { Income } from './Organisms/Income/Income';
+export { Expense } from './Organisms/Expense/Expense';
